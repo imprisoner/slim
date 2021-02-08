72dd0e9bc73ef3937050cc6aba0c53cd486d35c1
imprisoner <69797749+imprisoner@users.noreply.github.com>
2021-02-08 17:30:48 +0400

    fixed header

--- js/main.js
+++ js/main.js
@@ -13,12 +13,36 @@ const slider = new Swiper('.swiper-container', {
 
 document.addEventListener('click', (e) => {
     const map = document.querySelector('.map-section iframe')
-    console.log('event: ', e)
-    console.log(map)
-    console.log(e.target)
-    if(e.target.className === 'map-section') {
+    if (e.target.className === 'map-section') {
         map.style.pointerEvents = 'all'
     } else {
         map.style.pointerEvents = 'none'
     }
 })
+
+// fixed header 
+
+const header = document.querySelector('.header')
+const nav = document.querySelector('.header-nav').style
+let scroll = document.documentElement.scrollTop
+window.onscroll = () => {
+
+    if (document.documentElement.scrollTop >= 782) {
+        header.classList.add('fixed')
+        nav.padding = '5px 0'
+
+        if (document.documentElement.scrollTop < scroll) {
+            header.style.transition = 'ease 0.3s'
+            header.style.transform = 'translateY(0)'
+        } else {
+            header.style.transform = ''
+        }
+        scroll = document.documentElement.scrollTop
+    } else {
+        header.classList.remove('fixed')
+        nav.padding = ''
+        header.style.transform = ''
+        header.style.transition = ''
+    }
+
+}
